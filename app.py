a85d8202d7901c411fed5e9c5cbd296e42a751d4
ann1wu <annie.wu.1@stonybrook.edu>
2023-04-29 13:56:54 +0000

    updated html and py files

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask render_template()
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():
-    return 'render_template()'
+    return render_template('home.html')
 
-
-# app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
